5b6aac3ee9fef016f45ea19af095cee8fcab4a47
Emanuel Peire <emapeire@outlook.com>
2024-04-26 20:14:45 -0300

    Add styles.css import in index.js and update webpack configuration for CSS loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path')
 
+const rulesForStyles = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader']
+}
+
+const rulesForJavaScript = {
+  test: /\.js$/,
+  loader: 'babel-loader',
+  options: {
+    presets: [['@babel/preset-react', { runtime: 'automatic' }]]
+  }
+}
+
+const rules = [rulesForJavaScript, rulesForStyles]
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build')
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: [['@babel/preset-react', { runtime: 'automatic' }]]
-        }
-      }
-    ]
-  }
+  module: { rules }
 }
