52801f9fab2e673e3499153083e2e83490de74d4
Emanuel Peire <emapeire@outlook.com>
2024-04-26 19:42:37 -0300

    Init webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
