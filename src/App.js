2982388e8c7a1066c9eabd30ee70c8bbf19a12ca
Emanuel Peire <emapeire@outlook.com>
2024-04-26 21:15:46 -0300

    Update React import in index.js and add counter functionality in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from 'react'
+
 export default function App() {
+  const [counter, setCounter] = useState(0)
+  // const [values, setValues] = useState()
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    // setValues(values.concat(counter))
+  }
+
   return (
-    <div>
+    <div className='container'>
       <h1>Hello, world!</h1>
+      <button onClick={handleClick}>Click me!</button>
+      <p>Counter: {counter}</p>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const rulesForStyles = {
   test: /\.css$/,
@@ -15,10 +16,23 @@ const rulesForJavaScript = {
 
 const rules = [rulesForJavaScript, rulesForStyles]
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build')
-  },
-  module: { rules }
+module.exports = (_env, argv) => {
+  const { mode } = argv
+  const isProduction = mode === 'production'
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: isProduction ? '[name].[contenthash].js' : 'main.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+    module: { rules },
+    devServer: {
+      open: true,
+      port: 3000,
+      liveReload: true,
+      compress: true
+    },
+    devtool: 'source-map'
+  }
 }
